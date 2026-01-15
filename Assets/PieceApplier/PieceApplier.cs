26f34738d141f83d9023a055ac7d89d558c66182
exekiey <ezequielcanale@gmail.com>
2026-01-15 06:09:07 +0000

    funciona decision triple
    ahora no quiere funcionar la interacción con las bifurcaciones pero no sé eso, es cosa de Álvaro porque no toqué el script ese

--- Assets/PieceApplier/PieceApplier.cs
+++ Assets/PieceApplier/PieceApplier.cs
@@ -22,14 +22,39 @@ public class PieceApplier : MonoBehaviour
     [SerializeField] GameObject forkLeftRight;
     [SerializeField] GameObject forkLeftStraight;
     [SerializeField] GameObject forkRightStraight;
+    
+    [SerializeField] GameObject forkTriple;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     [SerializeField] GameObject upCorridor;
     [SerializeField] GameObject downCorridor;
     [SerializeField] GameObject straightCorridor;
 
+
+    List<Vector3> alreadyAppliedPieces;
+
+    PathNode lastNode;
+
+    void Start()
+    {
+        pathGraph = pathGenerator.graph;
+        mainPath = pathGraph.mainPath;
+        alreadyAppliedPieces = new List<Vector3>();
+
+        lastNode = mainPath[mainPath.Count - 1];
+
+        ApplyPiecesToPath(mainPath, 0);
+
+    }
+
     GameObject GetPieceBasedOnDirection(Vector3 previousPosition, Vector3 currentPosition, Vector3 nextPosition)
     {
+
+        previousPosition.y = 0;
+        currentPosition.y = 0;
+        nextPosition.y = 0;
+
         Vector3 prevSegment = (currentPosition - previousPosition).normalized;
         Vector3 nextSegment = (nextPosition - currentPosition).normalized;
 
@@ -50,34 +75,46 @@ public class PieceApplier : MonoBehaviour
         return new GameObject();
     }
 
-    GameObject GetForkPieceBasedOnDirection(
-        Vector3 previousPosition,
-        Vector3 currentPosition,
-        Vector3 nextPositionA,
-        Vector3 nextPositionB)
+    GameObject GetForkPieceBasedOnDirection(Vector3 previousPosition, Vector3 currentPosition, Vector3 nextPosition, Vector3 nextPositionA, Vector3 nextPositionB)
     {
-        Vector3 prevSegment = (currentPosition - previousPosition).normalized;
-        Vector3 nextA = (nextPositionA - currentPosition).normalized;
-        Vector3 nextB = (nextPositionB - currentPosition).normalized;
 
-        float angleA = Vector3.SignedAngle(prevSegment, nextA, Vector3.up);
-        float angleB = Vector3.SignedAngle(prevSegment, nextB, Vector3.up);
+        previousPosition.y = 0;
+        currentPosition.y = 0;
+        nextPosition.y = 0;
+        nextPositionA.y = 0;
+        nextPositionB.y = 0;
 
-        bool ALeft = angleA < -0.01f;
-        bool ARight = angleA > 0.01f;
-        bool AStraight = Mathf.Abs(angleA) <= 0.01f;
+        Vector3 prev = (currentPosition - previousPosition).normalized;
 
-        bool BLeft = angleB < -0.01f;
-        bool BRight = angleB > 0.01f;
-        bool BStraight = Mathf.Abs(angleB) <= 0.01f;
+        int left = 0;
+        int right = 0;
+        int straight = 0;
 
-        if ((ALeft && BRight) || (ARight && BLeft))
+        void Classify(Vector3 next)
+        {
+            if (next == Vector3.zero) return; // means no way out
+
+            float angle = Vector3.SignedAngle(prev, (next - currentPosition).normalized, Vector3.up);
+
+            if (angle < -0.01f) left++;
+            else if (angle > 0.01f) right++;
+            else straight++;
+        }
+
+        Classify(nextPosition);
+        Classify(nextPositionA);
+        Classify(nextPositionB);
+
+        if (left == 1 && right == 1 && straight == 1)
+            return forkTriple;   // triple bifurcation
+
+        if (left == 1 && right == 1)
             return forkLeftRight;
 
-        if ((ALeft && BStraight) || (AStraight && BLeft))
+        if (left == 1 && straight == 1)
             return forkLeftStraight;
 
-        if ((ARight && BStraight) || (AStraight && BRight))
+        if (right == 1 && straight == 1)
             return forkRightStraight;
 
         return null;
@@ -100,9 +137,10 @@ public class PieceApplier : MonoBehaviour
     {
         return (a - b).sqrMagnitude <= epsilon * epsilon;
     }
-    bool TryGetCurrentNodeSubPath(PathNode currentNode, out List<PathNode> subPath)
-    {
 
+    int TryGetCurrentNodeSubPaths(PathNode node, out List<PathNode> subPathA, out int indexA, out List<PathNode> subPathB, out int indexB)
+    {
+        /*
         foreach (List<PathNode> currentSubgraph in pathGraph.subPaths)
         {
             if (Approximately(currentSubgraph[0].position, currentNode.position))
@@ -112,41 +150,161 @@ public class PieceApplier : MonoBehaviour
             }
         }
         subPath = null;
+        return false;*/
+
+        subPathA = null;
+        subPathB = null;
+        indexA = -1;
+        indexB = -1;
+        
+        if (node.connections.Count <= 1)
+        {
+
+            return 0;
+        }
+
+        if (node.connections.Count == 3)
+        {
+
+            Debug.Log("una bifurcacion");
+            foreach (List<PathNode> currentSubgraph in pathGraph.subPaths)
+            {
+                Debug.Log("siguiente subgrafo");
+
+                for (int i = 0; i < currentSubgraph.Count; i++)
+                {
+                    PathNode currentNode = currentSubgraph[i];
+                    Debug.Log("siguiente nodo");
+
+                    if (Approximately(currentNode.position, node.position))
+                    {
+                        
+                        subPathA = currentSubgraph;
+                        subPathB = null;
+                        indexA = i;
+                        indexB = -1;
+
+                        return 1;
+                    }
+                }
+
+            }
+
+        }
+
+        if (node.connections.Count == 4)
+        {
+            foreach (List<PathNode> currentSubgraph in pathGraph.subPaths)
+            {
+
+
+                for (int i = 0; i < currentSubgraph.Count; i++)
+                {
+
+                    PathNode currentNode = currentSubgraph[i];
+
+                    if (Approximately(currentNode.position, node.position))
+                    {
+                        if (subPathA == null)
+                        {
+                            subPathA = currentSubgraph;
+                            indexA = i;
+                        }
+
+                        subPathB = currentSubgraph;
+                        indexB = i;
+                        Debug.Log("asda");
+                        return 2;
+                    }
+                }
+
+            }
+        }
+
+        return -1;
+
+    }
+    bool IsCurrentNodeAlreadyApplied(PathNode currentNode)
+    {
+        foreach (Vector3 currentAppliedPiecePosition in alreadyAppliedPieces)
+        {
+            if (Approximately(currentAppliedPiecePosition, currentNode.position))
+            {
+                return true;
+            }
+        }
+
+        alreadyAppliedPieces.Add(currentNode.position);
+
         return false;
     }
 
-    void Start()
+    private void ApplyPiecesToPath(List<PathNode> path, int start)
     {
-        pathGraph = pathGenerator.graph;
-        mainPath = pathGraph.mainPath;
-        ApplyPiecesToPath(mainPath);
-    }
 
-    private void ApplyPiecesToPath(List<PathNode> path)
-    {
-        for (int i = 1; i < path.Count - 1; i++)
+        PathNode currentNode = path[start + 1];
+        Debug.Log(currentNode.position);
+        Debug.Log(lastNode.position);
+        while (currentNode.position != lastNode.position)
         {
-            PathNode currentNode = path[i];
-            PathNode prevNode = path[i - 1];
-            PathNode nextNode = path[i + 1];
+            currentNode = currentNode.connections[1];
+            PathNode prevNode = currentNode.connections[0];
+            PathNode nextNode = currentNode.connections[1];
+
+            if (IsCurrentNodeAlreadyApplied(currentNode))
+            {
+                continue;
+            }
 
             Vector3 position = currentNode.position;
 
-            List<PathNode> subPath;
-            bool isCurrentNodeSubpath = TryGetCurrentNodeSubPath(currentNode, out subPath);
+            List<PathNode> subPathA;
+            List<PathNode> subPathB;
+            int indexA;
+            int indexB;
+            int numberOfSubPaths = TryGetCurrentNodeSubPaths(currentNode, out subPathA, out indexA, out subPathB, out indexB);
 
             Vector3 direction = (position - prevNode.position).normalized;
             float scaleFactor = (1 / 5f);
-            if (isCurrentNodeSubpath)
-            {
-                GameObject forkPiece = GetForkPieceBasedOnDirection(prevNode.position, position, nextNode.position, subPath[1].position);
 
+            if (numberOfSubPaths >= 1)
+            {
+
+                //solución horrible pero así se queda
+                Vector3 positionB = Vector3.zero;
+
+                if (subPathB != null)
+                {
+                    positionB = currentNode.connections[3].position;
+                }
+                //ahí termina la solución horrible
+
+                Debug.Log(prevNode.position);
+                Debug.Log(position);
+                Debug.Log(nextNode.position);
+                Debug.Log(subPathA);
+                Debug.Log(positionB);
+
+
+                GameObject forkPiece = GetForkPieceBasedOnDirection(
+                    prevNode.position, 
+                    position, 
+                    nextNode.position,
+                    currentNode.connections[2].position, 
+                    positionB);
                 GameObject forkPieceObject = Instantiate(forkPiece);
+
                 forkPieceObject.transform.forward = new Vector3(direction.x, 0, direction.z);
                 forkPieceObject.transform.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
                 forkPieceObject.transform.position = position;
-                ApplyPiecesToPath(subPath);
-            } else
+
+                ApplyPiecesToPath(subPathA, indexA);
+                if (numberOfSubPaths == 2)
+                {
+                    ApplyPiecesToPath(subPathB, indexB);
+                }
+            }
+            else
             {
 
                 GameObject currentPiece = GetPieceBasedOnDirection(prevNode.position, position, nextNode.position);
@@ -171,6 +329,8 @@ public class PieceApplier : MonoBehaviour
         }
     }
 
+
+
     // Update is called once per frame
     void Update()
     {
@@ -178,3 +338,4 @@ public class PieceApplier : MonoBehaviour
 
     }
 }
+
